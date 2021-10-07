48cac5ff30e3cac8e97303b7be049c73d1d84097
zdurr22 <zdurr22@gmail.com>
2021-10-07 14:42:03 -0400

    commit tree f9fe884938ad37173d910c837f52344decad5d49

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+const map = function(src, callback) {
+    let newArr = []
+    for (let i = 0; i < src.length; i++) {
+        newArr.push(callback(src[i]))
+    }
+    return newArr;
+}
+
+function reduce(src, callback, startingValue) {
+    let total;
+    if (startingValue) {
+        total = startingValue;
+        for (let i = 0; i < src.length; i++) {
+            total = callback(src[i], total);
+        }
+        return total;
+    } else {
+        total = src[0];
+        for (let i = 1; i < src.length; i++) {
+            total = callback(src[i], total);
+        }
+        return total;
+    }
+}
